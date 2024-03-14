739640530880f9946f15e04db03bc147aedee803
Dashevskii Dmitrii <rus51ddl@gmail.com>
2024-03-14 08:22:37 +0300

    created db.service, fixes in city.controllers

---
+++ server/src/db/db.service.ts
@@ -0,0 +1,29 @@
+import { join } from 'path'
+import util from 'util'
+import fs from 'fs'
+
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
+
+const databaseService = {
+  getFileData: async (fileName: string) => {
+    try {
+      const filePath = join(__dirname, fileName)
+      const data = await readFileAsync(filePath, 'utf-8')
+      return JSON.parse(data)
+    } catch (err) {
+      throw err
+    }
+  },
+  writeDataToFile: async (fileName: string, data: unknown) => {
+    try {
+      const filePath = join(__dirname, fileName)
+      await writeFileAsync(filePath, JSON.stringify(data), 'utf-8')
+      return true
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+export default databaseService
